773eefbed7cb603b26ae6ea56e68ecd8c77f7c28
Leticia de Godoy Enz <leticia.enz11@gmail.com>
2022-02-11 20:57:07 -0400

    create route courses

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import express from 'express';
+import express, { request } from 'express';
 
 const app =  express();
 
 app.get("/", (request, response) => {
     return response. json({ message: "hello Word"});
+});
+
+app.post("/courses", (request, reponse) => {
+    const { name } = request.body;
+    return reponse.json({ name });
 })
 
 

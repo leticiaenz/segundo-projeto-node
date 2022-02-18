5c21234918c27c2a93f017ea522a359b4bbddbd9
Leticia de Godoy Enz <leticia.enz11@gmail.com>
2022-02-17 22:43:07 -0400

    List Categories

--- routes/categories.routes.ts
+++ routes/categories.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { request, Router } from "express";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 
@@ -10,7 +10,12 @@ categoriesRoutes.post("/", (request, response) => {
 
     categoriesRepository.create({ name, description }); 
     return response.status(201).send();
+});
 
+categoriesRoutes.get("/", (request, response) => {
+    const all = categoriesRepository.list();
+    
+    return response.json(all);
 })
 
-export {categoriesRoutes };
+export { categoriesRoutes };

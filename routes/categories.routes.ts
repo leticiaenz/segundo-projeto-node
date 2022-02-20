6bdec81bfadefc80b34889c9d694d224de3ffd7a
Leticia de Godoy Enz <leticia.enz11@gmail.com>
2022-02-19 22:18:19 -0400

    Create service

--- routes/categories.routes.ts
+++ routes/categories.routes.ts
@@ -1,5 +1,6 @@
 import { request, Router } from "express";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../src/services/CreateCategoryService";
 
 
 const categoriesRoutes = Router();
@@ -8,13 +9,10 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    const categoryAlreadyExistis = categoriesRepository.findByName(name);
+    const createCategoryService =  new CreateCategoryService(categoriesRepository);
 
-    if(categoryAlreadyExistis) {
-        return response.status(400).json({ error: "Category Already existis" })
-    }
+    createCategoryService.execute({ name, description });
     
-    categoriesRepository.create({ name, description }); 
     return response.status(201).send();
 });
 
---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,24 @@
+import { CategoriesRepository } from "../../repositories/CategoriesRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+class CreateCategoryService {
+    constructor(private categoriesRepository): CategoriesRepository) {}
+
+    execute({ description, name }: IRequest): void {
+    
+        const categoryAlreadyExistis = this.categoriesRepository.findByName(name)
+
+        if(categoryAlreadyExistis) {
+            throw new Error("Category already exists");
+        }
+        
+        this.categoriesRepository.create({ name, description })
+    } 
+}
+
+
+export { CreateCategoryService };

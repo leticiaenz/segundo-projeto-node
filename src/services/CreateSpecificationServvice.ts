b880958ca15dfc864c5216fff35718b17ef1780c
Leticia de Godoy Enz <leticia.enz11@gmail.com>
2022-02-22 23:20:53 -0400

    Create Repository

--- src/services/CreateSpecificationServvice.ts
+++ src/services/CreateSpecificationServvice.ts
@@ -1,9 +1,27 @@
+import { ISpecificationsRepository } from "../modules/cars/repositories/ISpecificationsRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
 class CreateSpecificationService {
-    constructor() {
-        console.log("TODO");
+    constructor(private specificationsRepository: ISpecificationsRepository) {
+        
+    }
+    execute({ name, description }: IRequest): void {
+        const specificationAlreadyExists = this.specificationsRepository.findByName(name);
+
+        if(specificationAlreadyExists) {
+            throw new Error("Specification already exists");
+        }
+
+        this.specificationsRepository.create({
+            name,
+            description,
+        })
+
     }
 }
 
-
-
-export { CreateSpecificationService}
+export { CreateSpecificationService }

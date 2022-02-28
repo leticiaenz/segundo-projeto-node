045ef4b33b9c769df55d64d0e23006687eedc957
Leticia de Godoy Enz <leticia.enz11@gmail.com>
2022-02-28 10:54:52 -0400

    Create Use Case Specification

--- routes/specifications.routes.ts
+++ routes/specifications.routes.ts
@@ -1,18 +1,12 @@
 import { Router } from "express";
-import { SpecificationsRepository } from "../src/modules/cars/repositories/SpecificationsRepository";
-import { CreateSpecificationService } from "../src/modules/cars/services/CreateSpecificationServvice";
+import { createSpecificationController } from "../src/modules/cars/useCases/createSpecification";
 
 const specificationsRoutes = Router();
 
-const specificationRepository = new SpecificationsRepository();
 
 specificationsRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
-    const createSpecificationService = new CreateSpecificationService(specificationRepository);
-
-    createSpecificationService.execute({ name, description });
-
-    return response.status(201).send();
+    return createSpecificationController.handle(request, response);
+   
 })
 
 export { specificationsRoutes };

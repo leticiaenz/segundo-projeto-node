a8b3239c677647f716d49f77c4ca8aa92f770330
Leticia de Godoy Enz <leticia.enz11@gmail.com>
2022-03-04 11:38:43 -0400

    Read upload data

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,7 +1,9 @@
+import { CategoriesRepository } from "../../repositories/implementation/CategoriesRepository";
 import { ImportCategoryController } from "./ImportCategoryContoller";
 import { ImportCategoryUseCase } from "./importCategoryUseCase";
 
-const importCategoryUseCase =  new ImportCategoryUseCase()
+const categoriesRepository = CategoriesRepository.getIntance();
+const importCategoryUseCase =  new ImportCategoryUseCase(categoriesRepository)
 const importCategoryController = new ImportCategoryController(importCategoryUseCase);
 
 export { importCategoryController } ;

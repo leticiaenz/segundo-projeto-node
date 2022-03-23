bb05ef2ddb7b9955ace6dcdb7244893b304010a8
Leticia de Godoy Enz <leticia.enz11@gmail.com>
2022-03-23 17:17:49 -0400

    Create Service Specification

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,12 +1,20 @@
 import { Router } from "express";
+import { SpecificationsRepository } from "../modules/cars/repositories/implementation/SpecificationsRepository";
+import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
 import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationsRoutes = Router();
 
+const specificationsRepository = new SpecificationsRepository;
 
 specificationsRoutes.post("/", (request, response) => {
-    return createSpecificationController.handle(request, response);
+    const {name,  description } = request.body;
+    const createSpecificationService = new CreateSpecificationService(specificationsRepository);
    
-})
+    createSpecificationService.execute({ name, description});
+
+    return response.status(201);
+});
+
 
 export { specificationsRoutes };

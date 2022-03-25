9a44067b5693122f5256c70c0c7cddf5ace6215e
Leticia de Godoy Enz <leticia.enz11@gmail.com>
2022-03-25 15:43:26 -0400

    Fix router error

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,19 +1,16 @@
 import { Router } from "express";
-import { SpecificationsRepository } from "../modules/cars/repositories/implementation/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
 import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
+
+//import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+
 
 const specificationsRoutes = Router();
 
-const specificationsRepository = new SpecificationsRepository;
 
 specificationsRoutes.post("/", (request, response) => {
-    const {name,  description } = request.body;
-    const createSpecificationService = new CreateSpecificationService(specificationsRepository);
-   
-    createSpecificationService.execute({ name, description});
 
-    return response.status(201);
+
+    return  createSpecificationController.handle(request, response);
 });
 
 

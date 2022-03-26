2d3d435b2a9a2d85c2cdbefb8e0e90dcb46b6bb0
Leticia de Godoy Enz <leticia.enz11@gmail.com>
2022-03-26 11:19:10 -0400

    Fix swwager error

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -28,7 +28,7 @@ class ImportCategoryUseCase {
                 });
             })
             .on("end", () => {
-                fs.promises.unlink(file.path);
+               fs.promises.unlink(file.path);
                 resolve(categories);
             })
             .on("error", (err) => {
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,14 +1,9 @@
 import { Router } from "express";
 import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
-//import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
-
-
 const specificationsRoutes = Router();
 
-
 specificationsRoutes.post("/", (request, response) => {
-
 
     return  createSpecificationController.handle(request, response);
 });
